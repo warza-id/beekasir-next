94ea4bc95d2d065a1ff076ff4e6bb7ee1f4d5d8a
Kiki Kiswanto <kiki.kiswanto22@gmail.com>
2025-12-17 20:37:23 +0700

    [PAY] Log Debug

--- src/app/api/payment-dev/route.ts
+++ src/app/api/payment-dev/route.ts
@@ -6,14 +6,14 @@ import moment from "moment";
 import { DB_DEV } from "@/service/firebase";
 
 export async function POST(req : Request, response : Response, head : Headers) {
-  console.log("API Baru");
+  console.log("====> Payment DEV");
   
     try {
-      console.log("Payment Dev Receipt");
         
       const body = await req.json();
+
       console.log(body);
-      
+      console.log("====> Read Body");
       const headersList = headers();
       const apiKey  = headersList.get('user-agent');
       
@@ -22,11 +22,12 @@ export async function POST(req : Request, response : Response, head : Headers) {
       // }
       const refBill = doc(DB_DEV, "Billing/" + body.order_id);
       await updateDoc(refBill, body);
+      console.log("====> Update Billing");
       const bill = (await getDoc(refBill)).data();
       console.log(bill);
-
+      console.log("====> get Billing");
       if (body.transaction_status == 'settlement') {
-        
+        console.log("====> settlement");
         if (bill) {
           let exp = moment().add(bill.qty, 'M').format("YYYY-MM-DD");
           
@@ -45,22 +46,24 @@ export async function POST(req : Request, response : Response, head : Headers) {
               
           }
           
-          //console.log(exp);
+          console.log("====> Prepare Update Company");
           await updateDoc(refCompany, { 'level' : bill.level, 'exp' : exp});
+          console.log("====> Success Update Company");
           console.log(body.order_id + " - " + body.transaction_status + " Level Updated " + bill.level + " Until " + exp);
           return NextResponse.json({ 'data': 'Settlement and Upgrade Success ', 'status': '200', 'statusDesc' : 'Settlement Order '  + body.order_id});
         }
         
         console.log(body.order_id + " - " + body.transaction_status);
-        
+        console.log("====> Error Update");
         return NextResponse.json({ 'data': 'Settlement but Failed Upgrade', 'status': '200', 'statusDesc' : 'Order '  + body.order_id});
       }
       
       console.log(body.order_id + " - " + body.transaction_status);
+      console.log("====> Error Service");
       return NextResponse.json({ 'data': body, 'status': '200', 'statusDesc' : headersList});
     } catch (error) {
       console.log(error);
-      
+      console.log("====> Error Catch");
       return NextResponse.json({ 'data': "Internal Server Error", 'status': '500', 'statusDesc' : 'Please contact administrator'});
     }
   
